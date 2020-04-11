8891b9509a68fc2c16dfe7073855a96cdf9b0b2f
MikhaZyu <MikhaZyu@gmail.com>
2020-04-11 12:00:44 +0300

    Confirm component

---
+++ examples/BlazorWasmExamples/Pages/ConfirmExamples.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmExamples.Pages
+{
+    public partial class ConfirmExamples: ComponentBase
+    {
+        private List<string> _results = new List<string>();
+        private bool _isConfirmOpen;
+
+        private void OnConfirmShowClick()
+        {
+            _isConfirmOpen = true;
+        }
+
+        private void OnConfirmClose(bool answer)
+        {
+            _results.Add($"Answer: {answer} at {DateTime.Now}");
+            _isConfirmOpen = false;
+        }
+    }
+}
---
+++ src/BBComponents/BbConfirm.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBComponents
+{
+    public partial class BbConfirm: ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; } = "Question";
+
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string BtnOkText { get; set; } = "OK";
+
+        [Parameter]
+        public string BtnCancelText { get; set; } = "Cancel";
+
+        [Parameter]
+        public EventCallback<bool> OnClose { get; set; }
+
+        private async Task OnCancelClick()
+        {
+            await OnClose.InvokeAsync(false);
+        }
+
+        private async Task OnOkClick()
+        {
+            await OnClose.InvokeAsync(true);
+        }
+    }
+}
