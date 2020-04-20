0782de87a78a1e57a02cde9c33c11866778d4e05
MikhaZyu <MikhaZyu@gmail.com>
2020-04-20 19:35:29 +0300

    List group examples

---
+++ examples/BlazorWasmExamples/Pages/ListGroupExamples.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWasmExamples.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmExamples.Pages
+{
+    public partial class ListGroupExamples: ComponentBase
+    {
+        private List<Currency> _currencies;
+
+        protected override void OnInitialized()
+        {
+            _currencies = Currency.SampleData().ToList();
+
+        }
+    }
+}
