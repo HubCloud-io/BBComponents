08fe0b40e292308f8ce58cabadfdd9f1f79566fc
MikhaZyu <MikhaZyu@gmail.com>
2020-05-14 18:02:51 +0300

    Model for list item

---
+++ src/BBComponents/Models/ListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BBComponents.Models
+{
+    public class ListItem<TValue>
+    {
+        public TValue Value { get; set; }
+        public bool IsSelected { get; set; }
+        public bool IsVisible { get; set; }
+
+        public ListItem()
+        {
+
+        }
+
+        public ListItem(TValue value)
+        {
+            Value = value;
+        }
+    }
+}
