8b4e6c123e83ca061f0d8884d5da2cd82c1026b0
MikhaZyu <MikhaZyu@gmail.com>
2021-01-27 14:30:56 +0300

    Collapsible group component

---
+++ src/BBComponents/Components/CollapsibleGroup.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BBComponents.Components
+{
+    public partial class CollapsibleGroup<TValue>: ComponentBase
+    {
+        private bool _isOpen = true;
+
+        [Parameter]
+        public string HtmlClass { get; set; }
+
+        [Parameter]
+        public string HtmlStyle { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public TValue DataItem { get; set; }
+
+        [Parameter]
+        public bool Open { get; set; }
+
+        [Parameter]
+        public RenderFragment<TValue> ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _isOpen = Open;
+        }
+
+        private void OnTitleClicked()
+        {
+            _isOpen = !_isOpen;
+        }
+    }
+}
---
+++ test/BlazorServerSideForDebug/Pages/CollapsibleGroupTests.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorServerSideForDebug.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSideForDebug.Pages
+{
+    public partial class CollapsibleGroupTests: ComponentBase
+    {
+        private Currency _currency;
+
+        protected override void OnInitialized()
+        {
+            _currency = new Currency { Id = 1, CodeNumeric = "978", Code = "EUR", Title = "Euro" };
+        }
+    }
+}
