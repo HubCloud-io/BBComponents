581fccca62cafafa9be7b9a68b428ac89924e0f6
MikhaZyu <MikhaZyu@gmail.com>
2021-12-14 21:34:50 +0300

    Clear date

--- src/BBComponents/Components/BbDatePicker.razor.cs
+++ src/BBComponents/Components/BbDatePicker.razor.cs
@@ -1,4 +1,5 @@
-﻿using BBComponents.Enums;
+﻿using BBComponents.Abstract;
+using BBComponents.Enums;
 using BBComponents.Helpers;
 using BBComponents.Models;
 using Microsoft.AspNetCore.Components;
@@ -26,6 +27,13 @@ namespace BBComponents.Components
 
         private CultureInfo _culture;
         private List<List<CalendarDay>> _weeks;
+
+        private bool _isCustomMenuOpen;
+        private double _clientX;
+        private double _clientY;
+
+        private List<IMenuItem> _menuItems = new List<IMenuItem>();
+
 
         [Inject]
         public IJSRuntime JsRuntime { get; set; }
@@ -85,7 +93,11 @@ namespace BBComponents.Components
         public string HtmlClass { get; set; }
 
         [Parameter]
-        public int DropdownWidth { get; set; } = 230;
+        public int DropdownWidth { get; set; } = 210;
+
+        [Parameter]
+        public bool UseCustomMenu { get; set; }
+
 
         public string SizeClass => HtmlClassBuilder.BuildSizeClass("input-group", Size);
 
@@ -189,6 +201,29 @@ namespace BBComponents.Components
         protected override void OnInitialized()
         {
             _culture = Thread.CurrentThread.CurrentCulture;
+
+            _menuItems = new List<IMenuItem>();
+
+            _menuItems.Add(new MenuItem()
+            {
+                Title = $"Clear",
+                Name = $"clear",
+                Kind = MenuItemKinds.Item,
+                IconClass = "fa fa-times text-danger",
+                HotKeyTooltip = "Alt+X"
+
+            });
+
+            _menuItems.Add(new MenuItem() { Kind = MenuItemKinds.Delimiter });
+
+            _menuItems.Add(new MenuItem()
+            {
+                Title = $"Close",
+                Name = $"close",
+                Kind = MenuItemKinds.Item,
+                IconClass = "fa fa-times text-secondary"
+            });
+
         }
 
         protected override void OnParametersSet()
@@ -287,6 +322,13 @@ namespace BBComponents.Components
 
             _stringValue = e.Value?.ToString();
 
+            if (string.IsNullOrWhiteSpace(_stringValue))
+            {
+                await Clear();
+
+                return;
+            }
+
             if (DateTime.TryParseExact(_stringValue, Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateTime))
             {
                 Value = dateTime;
@@ -300,6 +342,62 @@ namespace BBComponents.Components
             {
                 _stringValue = Value.ToString(Format);
             }
+        }
+
+        private async Task OnInputKeyDown(KeyboardEventArgs e)
+        {
+           
+            if (e.AltKey == true && e.Code == "KeyX")
+            {
+
+                if (IsDisabled)
+                {
+                    return;
+                }
+
+                await Clear();
+
+            }
+          
+        }
+
+        private void OnContextMenu(MouseEventArgs e)
+        {
+            if (!UseCustomMenu)
+            {
+                return;
+            }
+
+            _clientX = e.ClientX;
+            _clientY = e.ClientY;
+
+            _isCustomMenuOpen = true;
+
+        }
+
+        private async Task OnContextMenuClosed(IMenuItem item)
+        {
+            _isCustomMenuOpen = false;
+
+            if (item.Name == "clear")
+            {
+                if (IsDisabled)
+                {
+                    return;
+                }
+
+                await Clear();
+            }
+          
+
+        }
+
+        private async Task Clear()
+        {
+            Value = DateTime.MinValue;
+            _stringValue = "";
+            await ValueChanged.InvokeAsync(Value);
+            await Changed.InvokeAsync(Value);
         }
 
         public static DateTime GetFirstCalendarDate(DateTime value, FirstWeekDays firstWeekDay)
