f3d939d8376b8a590005d68b49242acaa855f3e7
Michael Zyubin <MikhaZyu@gmail.com>
2023-01-19 13:05:50 +0300

    Set current item of list

---
+++ test/BlazorServerSideForDebug/Pages/ListTestsPage.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BlazorServerSideForDebug.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerSideForDebug.Pages;
+
+public partial class ListTestsPage: ComponentBase
+{
+    private List<Currency> _currencies;
+    private Currency _selectedItem;
+    
+    protected override void OnInitialized()
+    {
+        _currencies = Currency.SampleData().ToList();
+
+    }
+
+    private void OnSetItemClick()
+    {
+         _selectedItem = _currencies[1];
+    }
+
+    private void OnItemClick(Currency item)
+    {
+        _selectedItem = item;
+    }
+
+}
